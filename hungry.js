c054fd99a31912de0a909de79f15e30ecd2ff69d
Imran Hosen <imranh.pranto@gmail.com>
2021-02-08 17:17:27 +0600

    Half Work done

--- hungry.js
+++ hungry.js
@@ -1,13 +1,24 @@
-
-
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+const searchMeal = () =>{
+    const inputMeal = document.getElementById('input-meal').value;
+    // console.log(inputMeal);
+    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputMeal}`;
+    console.log(url);
+    fetch(url)
     .then(res => res.json())
     .then(data => displayMeals(data.meals))
+}
 
-    const displayMeals = meals =>{
-        for(let i=0; i> meals.length; i++){
-        const item = meals[i];
-        console.log(item.strMeal);
-        
-        }
-    }
+const displayMeals = meals =>{
+    const foodInfoDiv = document.getElementById('food-info')
+    meals.forEach(show => {
+        // console.log(show.strMeal);
+    const mealsDiv = document.createElement('div');
+    mealsDiv.className = 'mealsItem';
+    mealsDiv.innerHTML = `
+    <img class='mealsImg' src="${show.strMealThumb}">
+    <h3>${show.strMeal}</h3>
+    
+    `
+    foodInfoDiv.appendChild(mealsDiv);
+    });
+}
